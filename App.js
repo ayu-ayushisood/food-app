df7544972740c5ce8f40a356e84f7ce4d75800ba
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-08-09 20:04:53 +0530

    add load food api

--- App.js
+++ App.js
@@ -9,12 +9,18 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native'
 
+const loadFood = async function(){
+	const res = await fetch('https://api.jsonbin.io/b/5f2c36626f8e4e3faf2cb42e');
+	const data = await res.json();
+	console.log(data)
+}
+
 const App = () => (
-  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <TouchableOpacity> 
-      <Text>Launch</Text> 
-    </TouchableOpacity>
-  </View>
+	<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+		<TouchableOpacity onPress={ () => {loadFood()} }> 
+			<Text>Launch</Text> 
+		</TouchableOpacity>
+  	</View>
 )
 
 export default App
