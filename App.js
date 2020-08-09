b5587a3d7dd604dad6e2e3fd4c7de5d69afbdd4c
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-08-09 19:45:48 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native'
+
+const App = () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <TouchableOpacity> 
+      <Text>Launch</Text> 
+    </TouchableOpacity>
+  </View>
+)
+
+export default App
+
