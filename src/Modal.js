f2e941fb551c3c88ef8f7b96021bd5ffe285e567
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-08-18 23:25:53 +0530

    let search by title

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, TouchableHighlight, SectionList, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableHighlight, SectionList, TouchableOpacity, TextInput} from 'react-native';
 import styles from '../common/styles';
 import SubCategory from './SubCategory'
 import Category from './Category'
@@ -9,7 +9,8 @@ class ModalView extends React.Component {
   constructor(props){
     super(props);
     this.state= {
-      sectionsData: []
+			sectionsData: [],
+			matchedItemsArray: []
     }
   }
 
@@ -19,7 +20,6 @@ class ModalView extends React.Component {
 
   updateStateDataFromApi = (foodData) => {
     let sectionsData = foodData.map((obj) => {
-      console.log("object", obj)
       return {
         title: obj.category.categoryName,
         data: obj.category.subcategories,
@@ -28,39 +28,61 @@ class ModalView extends React.Component {
       };
     });
 
-    this.setState({sectionsData});
+    this.setState({sectionsData, matchedItemsArray: sectionsData});
   };
 
-  collapse(name){
+  collapse = (name) => {
     let stateObj = {};
     stateObj[`${name}`] = !this.state[`${name}`];
-    this.setState(stateObj, ()=> {console.log("state::", this.state)})
-    console.log("state obj", stateObj)
+    this.setState(stateObj)
+  }
+
+  searchFood = (searchQuery) => {
+    let sectionsData = this.state.sectionsData
+		let matchedItemsArray = [];
+		
+    searchQuery == '' ? 
+      this.setState({matchedItemsArray: sectionsData})
+    :
+      this.setState({matchedItemsArray: sectionsData}, () => {
+        sectionsData.map((item) => {
+					console.log(item)
+          if(item.title.includes(searchQuery)){
+            matchedItemsArray.push(item)
+          }
+        })
+        this.setState({matchedItemsArray})
+      })
   }
 
   render() {
     return (
       <View style={styles.modalView}>
         <TouchableHighlight
-          onPress={ () => {
-            this.props.setModalVisible(false);
-          }}>
-          <Text style={{fontSize: 25, marginTop: 20}}>X</Text>
+					onPress={ () => {
+						this.props.setModalVisible(false);
+				}}>
+				<Text style={{fontSize: 25, marginTop: 20}}>X</Text>
         </TouchableHighlight>
-        <Text style={{fontSize: 25, marginBottom: 20, fontWeight: '600'}}>Approved Foods List</Text>
+        <Text style={styles.headline}>Approved Foods List</Text>
+        <TextInput 
+					onChangeText={(item) => {this.searchFood(item)}} 
+					placeholder="Try searching fat, sauces, names..."
+					style={styles.searchInput}
+          />   
         <View>
-          <SectionList
-            sections={ this.state.sectionsData }
-            keyExtractor={(item, index) => item + index }
-            
-            renderSectionHeader={ ({section: {title, categoryData}}) => (
-              <TouchableOpacity style={styles.categoryContainer} onPress={ ()=> this.collapse(title)} >
-                <Category category={title} colorCode={categoryData.colorCode} servingSize={categoryData.servingSize} />
-              </TouchableOpacity>
-            )}
+			<SectionList
+				sections={ this.state.matchedItemsArray }
+				keyExtractor={(item, index) => item + index }
+				
+				renderSectionHeader={ ({section: {title, categoryData}}) => (
+					<TouchableOpacity style={styles.categoryContainer} onPress={ ()=> this.collapse(title)} >
+						<Category category={title} colorCode={categoryData.colorCode} servingSize={categoryData.servingSize} />
+					</TouchableOpacity>
+				)}
 
-            renderItem={ ({item, title, section}) => this.state[`${section.title}`] ? <SubCategory items={item}/> : null}
-          />
+				renderItem={ ({item, section}) => this.state[`${section.title}`] ? <SubCategory items={item}/> : null}
+			/>
         </View>
       </View>
     );
